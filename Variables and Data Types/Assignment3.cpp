0ecf13ae260cd92514fd8952242d435d4a44a5ab
Madhav Gupta <madhavgupta5555@gmail.com>
2023-01-16 18:32:07 +0530

    Write a C++ program to take length and breadth of a rectangle and print its area.

---
+++ Variables and Data Types/Assignment3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float length, breadth;
+    cout<<"Enter length of rectangle: ";
+    cin>>length;
+    cout<<"Enter breadth of rectangle: ";
+    cin>>breadth;
+    float area;
+    area = length * breadth;
+    cout<<"Area of rectangle is: "<<area;
+    return 0;
+}
